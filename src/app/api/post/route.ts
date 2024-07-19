a45cc20100a17194e16cf13caf42005037ceaf88
suisan0731 <suisan0731@gmail.com>
2024-07-20 06:13:08 +0900

    playlist postを追加

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+}
+
+export async function POST(request: NextRequest) {
+    
+}
+
+export async function PUT(request: NextRequest) {
+    
+}
+
+export async function DELETE(request: NextRequest) {
+    
+}
