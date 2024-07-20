ccd755ad39a051e4ae3c12b99210bb89efebfab3
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-21 02:35:13 +0900

    よくみたら違ってた

--- workspace/src/app/api/music/track/search/route.ts
+++ workspace/src/app/api/music/track/search/route.ts
@@ -6,8 +6,8 @@ import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
     const {search} = await (request.json() as Promise<SearchBody>)
     if(search === null) throw new Error("Query Not Set.")
-    const result = await SpotifySDK.search(search,["artist"],"JP",5)
+    const result = await SpotifySDK.search(search,["track"],"JP",5)
     return Response.json({
-        artists: result.artists.items
+        artists: result.tracks.items
     })
 }
