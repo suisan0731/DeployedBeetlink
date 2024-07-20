ee156a2fa1a4e85b1ff4503ebe1f5994c1a6d43d
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-20 11:34:38 +0900

    なんとかした

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import {auth} from "@/auth"
 
 export default auth(req => {
     if(!req.auth){
-        return Response.redirect(new URL("/",req.url))
+        return Response.redirect(new URL("/",req.nextUrl))
     }
 })
 
