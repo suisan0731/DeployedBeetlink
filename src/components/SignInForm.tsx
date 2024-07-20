ec682281edf21a77dd32cd5b3a2a569dc072d930
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-20 11:16:39 +0900

    こうかい？

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,12 +1,11 @@
 import { auth, signIn } from "@/auth";
-import { redirect } from "next/navigation";
 
 export function SignIn() {
     return (
     <form
         action={async () => {
             "use server"
-            await signIn(undefined,{redirectTo: "/loggedin"});
+            await signIn("google",{redirectTo: "/loggedin"});
         }}
         >
         <button className="btn" type="submit">Sign in</button>
