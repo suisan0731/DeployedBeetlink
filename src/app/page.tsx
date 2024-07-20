f0f1562dc4f365b6ba9ba38622d6e183ae1fd12d
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-20 12:12:07 +0900

    ある程度は置き替えた！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import LoginProvider from "@/compoents/LoginProvider";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Unauthenticated, Authenticated } from "convex/react";
+
 export default function Home() {
   return (
-    <></>
+    <LoginProvider>
+      <main>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+        <p>test</p>
+      </Authenticated>
+    </main>
+    </LoginProvider>
   );
 }
