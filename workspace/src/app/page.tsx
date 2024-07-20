a8725643ce57780d8faec17cf4dd657e4f57ac4b
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-20 18:16:58 +0900

    これで治るかな

--- workspace/src/app/page.tsx
+++ workspace/src/app/page.tsx
@@ -1,19 +1,35 @@
-import LoginProvider from "@/components/LoginProvider";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Unauthenticated, Authenticated } from "convex/react";
+"use client";
+
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
 
 export default function Home() {
+ const { isAuthenticated, isLoading } = useConvexAuth();
+ const { user } = useUser();
+ console.log(JSON.stringify(user,null,2))
+
+ if (isLoading) return <div>Loading...</div>;
+
   return (
-    <LoginProvider>
-      <main>
-      <Unauthenticated>
-        <SignInButton />
-      </Unauthenticated>
-      <Authenticated>
-        <UserButton />
-        <p>test</p>
-      </Authenticated>
-    </main>
-    </LoginProvider>
+    <div className="h-screen flex items-center justify-center flex-col gap-y-4">
+      <h1 className="text-xl font-semibold">
+        ようこそ！{isAuthenticated ? user?.fullName : "ゲスト"}さん
+      </h1>
+      <div className="flex gap-4">
+        {!isAuthenticated ? (
+          <SignInButton mode="modal">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              ログイン
+            </button>
+          </SignInButton>
+        ) : (
+          <SignOutButton>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              ログアウト
+            </button>
+          </SignOutButton>
+        )}
+      </div>
+    </div>
   );
 }
