d7d737256774e6c35be911c8bb5f310c0f5134ba
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-21 01:57:07 +0900

    artistのポイントになってなかったのを今きづいた

--- workspace/src/app/api/music/artist/search/route.ts
+++ workspace/src/app/api/music/artist/search/route.ts
@@ -5,8 +5,8 @@ import { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
     const {search} = await (request.json() as Promise<SearchBody>)
     if(search === null) throw new Error("Query Not Set.")
-    const tracks = await SpotifySDK.search(search,["track"])
+    const tracks = await SpotifySDK.search(search,["artist"],"JP",5)
     return Response.json({
-        tracks: tracks.tracks.items
+        tracks: tracks.artists.items
     })
 }
