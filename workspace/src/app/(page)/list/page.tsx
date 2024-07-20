c1567ae692455fda6d06194b20e8125d9a00a7cb
ogaoga08 <kamekameshin0514@gmail.com>
2024-07-21 02:02:06 +0900

    ナビゲーションバーの設置

--- workspace/src/app/(page)/list/page.tsx
+++ workspace/src/app/(page)/list/page.tsx
@@ -1,65 +1,68 @@
-
-"use client";
-
-import React from "react";
-import IframeCard from "../../../components/ifamecard";
-import { useRouter } from "next/navigation";
-import TopButton from "../../../components/top-button";
+"use client"
+import { useRouter } from "next/navigation"
+import NavBar from "../../../components/NavBar"
+import IframeCard from "../../../components/ifamecard"
 
 const Page = () => {
-  const router = useRouter();
-  const cardData = [
-    {
-        musicUrl: "https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0",
-        commentTitle: "Comment for Music Title 1",
-    },
-    {
-        musicUrl: "https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0",
-        commentTitle: "Comment for Music Title 2",
-    },
-    {
-        musicUrl: "https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0",
-        commentTitle: "Comment for Music Title 3",
-    },
-  ];
+	const Router = useRouter()
+	const CardData = [
+		{
+			musicUrl:
+				"https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0",
+			commentTitle: "Comment for Music Title 1",
+		},
+		{
+			musicUrl:
+				"https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0",
+			commentTitle: "Comment for Music Title 2",
+		},
+		{
+			musicUrl:
+				"https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0",
+			commentTitle: "Comment for Music Title 3",
+		},
+	]
 
-  return (
-    <div className="outer-container h-screen bg-white">
-      <div className="flex justify-center px-5 safari-hack">
-        <div className="max-w-screen-lg p-8">
-          <TopButton title="投稿一覧" />
+	return (
+		<div className="outer-container h-screen bg-white">
+			<NavBar />
+			<div className="flex justify-center px-5 safari-hack">
+				<div className="max-w-screen-lg p-8">
+					<div className="flex flex-wrap justify-center">
+						{CardData.map((Card, Index) => (
+							<IframeCard
+								key={Index}
+								musicUrl={Card.musicUrl}
+								commentTitle={Card.commentTitle}
+							/>
+						))}
+					</div>
 
-          <div className="flex flex-wrap justify-center">
-            {cardData.map((card, index) => (
-              <IframeCard
-                key={index}
-                musicUrl={card.musicUrl}
-                commentTitle={card.commentTitle}
-              />
-            ))}
-          </div>
+					{/*ペジネーション*/}
+					<div className="mb-10" />
+					<div className="flex justify-center mt-4">
+						<div className="join">
+							<button className="join-item btn">1</button>
+							<button className="join-item btn">2</button>
+							<button className="join-item btn btn-disabled">...</button>
+							<button className="join-item btn">99</button>
+							<button className="join-item btn">100</button>
+						</div>
+					</div>
+					<div className="mb-10" />
+					<div className="flex justify-center">
+						<button
+							className="btn btn-outline w-60"
+							type="button"
+							onClick={() => Router.back()}
+						>
+							戻る
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-          {/*ペジネーション*/}
-          <div className="mb-10" />
-          <div className="flex justify-center mt-4">
-            <div className="join">
-              <button className="join-item btn">1</button>
-              <button className="join-item btn">2</button>
-              <button className="join-item btn btn-disabled">...</button>
-              <button className="join-item btn">99</button>
-              <button className="join-item btn">100</button>
-            </div>
-          </div>
-          <div className="mb-10" />
-          <div className="flex justify-center">
-            <button className="btn btn-outline w-60" type="button" onClick={() => router.back()}>
-                  戻る
-            </button> 
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Page;
+export default Page
--- workspace/src/app/(page)/play/page.tsx
+++ workspace/src/app/(page)/play/page.tsx
@@ -1,40 +1,41 @@
-"use client";
-
-import React from 'react';
-import { useRouter } from "next/navigation";
-import TopButton from "../../../components/top-button";
+"use client"
+import { useRouter } from "next/navigation"
+import NavBar from "../../../components/NavBar"
 
 const Page = () => {
-  const router = useRouter();
+	const Router = useRouter()
 
-  return (
-    <div className="outer-container h-screen bg-white">
-      <div className="max-w-screen-lg w-full p-8">
-        <TopButton title="" />
+	return (
+		<div className="outer-container h-screen bg-white">
+			<NavBar />
+			<div className="max-w-screen-lg w-full p-8">
+				<div className="flex justify-center items-center mb-4">
+					<iframe
+						className="rounded-lg"
+						style={{ borderRadius: "12px" }}
+						src="https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0"
+						width="100%"
+						height="352"
+						allowFullScreen
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+						loading="lazy"
+						title="Spotify Embed"
+					></iframe>
+				</div>
 
-        <div className="flex justify-center items-center mb-4">
-          <iframe
-            className="rounded-lg"
-            style={{ borderRadius: '12px' }}
-            src="https://open.spotify.com/embed/track/78W4mTLIh4qoLu92W4IQhO?utm_source=generator&theme=0"
-            width="100%"
-            height="352"
-            allowFullScreen
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-            title="Spotify Embed"
-          ></iframe>
-        </div>
+				{/* ボタン */}
+				<div className="flex flex-col space-y-4 items-center">
+					<button
+						className="btn btn-outline w-60"
+						type="button"
+						onClick={() => Router.back()}
+					>
+						戻る
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-        {/* ボタン */}
-        <div className="flex flex-col space-y-4 items-center">
-          <button className="btn btn-outline w-60" type="button" onClick={() => router.back()}>
-            戻る
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Page;
+export default Page
