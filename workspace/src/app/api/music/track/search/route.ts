8275ee7f109cd02a4214180afdd154a578d1f89f
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-21 02:47:37 +0900

    本当に必要なデータだけ返す

--- workspace/src/app/api/music/track/search/route.ts
+++ workspace/src/app/api/music/track/search/route.ts
@@ -8,6 +8,11 @@ export async function POST(request: NextRequest) {
     if(search === null) throw new Error("Query Not Set.")
     const result = await SpotifySDK.search(search,["track"],"JP",5)
     return Response.json({
-        tracks: result.tracks.items
+        tracks: result.tracks.items.map((track) => {
+            return {
+                name: track.name,
+                id: track.id
+            }
+        })
     })
 }
