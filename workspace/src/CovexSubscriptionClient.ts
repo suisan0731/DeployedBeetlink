52c94f7024a499bb72917562fd361bd1aee495ca
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-20 23:33:30 +0900

    こうなのか？

--- workspace/src/CovexSubscriptionClient.ts
+++ workspace/src/CovexSubscriptionClient.ts
@@ -1,4 +1,4 @@
 import { ConvexClient } from "convex/browser";
 
-const convex_client = new ConvexClient(process.env.NODE_ENV === "development" ? process.env.CONVEX_SITE_URL : process.env.NEXT_PUBLIC_VERCEL_URL)
+const convex_client = new ConvexClient(process.env.NODE_ENV === "development" ? process.env.CONVEX_SITE_URL : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`)
 export default convex_client
