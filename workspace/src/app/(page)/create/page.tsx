5c2bb5f061cf696c1b94db444d52a9874f2a6332
suisan0731 <suisan0731@gmail.com>
2024-07-20 17:52:53 +0900

    cssのパス修正

--- workspace/src/app/(page)/create/page.tsx
+++ workspace/src/app/(page)/create/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import "../style.css";
+import "../../globals.css";
 import { useRouter } from "next/navigation";
 
 const Page = () => {
--- workspace/src/app/(page)/show/page.tsx
+++ workspace/src/app/(page)/show/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import "../style.css"
+import "../../globals.css";
 import { couldStartTrivia } from 'typescript';
 import { useRouter } from "next/navigation";
 
