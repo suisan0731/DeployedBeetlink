3a08038f841066d09180ff0b30094433cabb5496
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-21 03:06:46 +0900

    POSTメソッドに修正

--- workspace/src/hooks/useTracks.ts
+++ workspace/src/hooks/useTracks.ts
@@ -1,12 +1,11 @@
-import { SearchBody } from "@/app/api/body";
-import useExtendSWR from "./useExtendSWR";
 import useSWR from "swr";
 
 const useTracks = (search: string) => {
     return useSWR("/api/music/track/search",(url) => fetch(url,{
         body: JSON.stringify({
             search: search
-        })
+        }),
+        method: "POST"
     }).then(res => (res.json() as Promise<{tracks: {
         name: string,
         id: string
