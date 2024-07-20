70246807a15df617a9bcebd82f249d360d739718
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-07-20 23:31:31 +0900

    これでうまくいかなかったらもう困る

--- workspace/env.d.ts
+++ workspace/env.d.ts
@@ -8,16 +8,21 @@ declare module "process" {
                 NEXT_PUBLIC_CONVEX_URL: string,
                 /** @param デプロイ先のURL */
                 CONVEX_SITE_URL: string,
+
                 /** @param SpotifyのAPIを使うためのID */
                 SPOTIFY_CLIENT_ID: string,
                 /** @param SpotifyのAPIを使うための隠しトークン */
                 SPOTIFY_CLIENT_SECRET: string,
+
                 /** @param ClerkのIssuerのURL */
                 CLERK_ISSUER_URL: string,
                 /** @params CLERKのPublish Key */
                 NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string,
                 /** @param ClerkのSecretKey */
                 CLERK_SECRET_KEY: string
+
+                /** @param Vercelにデプロイした先の公開URL */
+                NEXT_PUBLIC_VERCEL_URL
             }
         }
     }
